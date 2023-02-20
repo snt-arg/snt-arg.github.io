295b438ad98d9ebd1faddc07fe17d63b254e338e
Maria Zhekova <mzhekova97@gmail.com>
2023-02-20 15:45:37 +0100

    add new tabs

---
+++ wp-content/themes/oshin/headers/header-hamburger.php
@@ -0,0 +1,5 @@
+<span class="be-mobile-menu-icon ">   
+        <span class="hamburger-line-1"></span>
+        <span class="hamburger-line-2"></span>
+        <span class="hamburger-line-3"></span>
+</span>
